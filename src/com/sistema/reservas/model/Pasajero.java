9263ec5374517ffbd6a4c7e7bb4874fbe2d00ace
María de los Ángeles Gómez Quisoboni <m.gomez@unimayor.edu.co>
2026-09-05 22:55:49 -0500

    Nueva modificación clase Pasajero que no había subido anteriormente

--- src/com/sistema/reservas/model/Pasajero.java
+++ src/com/sistema/reservas/model/Pasajero.java
@@ -1,3 +1,24 @@
 public class Pasajero {
     
+    private String cedula;
+    private String nombre;
+    private String correo;
+
+    public Pasajero (String cedula, String nombre, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
 }
