bb6f74c39974c10a0ac59390cab4f7a1ad4ed4b0
nelcebu <n.escobar@unimayor.edu.co>
2026-09-07 21:15:28 -0500

    creacion de las listas en GestorReservas

--- src/com/sistema/reservas/model/GestorReservas.java
+++ src/com/sistema/reservas/model/GestorReservas.java
@@ -1,5 +1,27 @@
 package com.sistema.reservas.model;
 
-public class GestorReserva {
+import com.sistema.reservas.model.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class GestorReservas {
+
+    private List<Vuelo> vuelos;
+    private List<Reserva> reservas;
+    private List<Pasajero> pasajeros;
+
+    public GestorReservas(){
+        this.vuelos = new ArrayList<>();
+        this.reservas = new ArrayList<>();
+        this.pasajeros = new ArrayList<>();
+    }
+    public void registrarPasajero(Pasajero pasajero){
+        pasajeros.add(pasajero);
+    }
+
+    public void registrarVuelo(Vuelo vuelo){
+        vuelos.add(vuelo);
+    }
 
 }
